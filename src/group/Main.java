60b7902e4a6c7fdd6c65d2175fd4e77e97d44eaa
joefontaine84 <jfont84@wgu.edu>
2023-04-28 04:10:22 -0600

    Second commit

---
+++ src/group/Main.java
@@ -0,0 +1,25 @@
+package group;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        JDBC.makeConnection();
+        launch(args);
+        JDBC.closeConnection();
+    }
+}

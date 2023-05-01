b83afdab8f40557dbe97edf713a10e1dae1ff18d
joefontaine84 <jfont84@wgu.edu>
2023-05-01 17:04:59 -0600

    10th Commit

--- src/group/controller/AppointmentsController.java
+++ src/group/controller/AppointmentsController.java
@@ -1,10 +1,57 @@
 package group.controller;
 
+import group.dao.Data;
+import group.model.Appointments;
+import javafx.beans.property.Property;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ResourceBundle;
+
+import static group.model.Appointments.apptsList;
+
 /**
  * This class provides various functionality for the AppointmentsView FXML page related to the GUI pane observed
  * by the user after logging in.
  * */
-public class AppointmentsController {
+public class AppointmentsController implements Initializable {
+
+    public TableView<Appointments> apptsTableView;
+    public TableColumn<Appointments, Integer> appointmentID;
+    public TableColumn<Appointments, String> title;
+    public TableColumn<Appointments, String> description;
+    public TableColumn<Appointments, String> location;
+    public TableColumn<Appointments, String> type;
+    public TableColumn<Appointments, Timestamp> startDateTime;
+    public TableColumn<Appointments, Timestamp> endDateTime;
+    public TableColumn<Appointments, Integer> customerID;
+    public TableColumn<Appointments, Integer> userID;
+    public TableColumn<Appointments, Integer> contactID;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            Data.populateAppointments();
+            apptsTableView.setItems(apptsList);
+            appointmentID.setCellValueFactory(new PropertyValueFactory<Appointments, Integer>("appointmentID"));
+            title.setCellValueFactory(new PropertyValueFactory<Appointments, String>("title"));
+            description.setCellValueFactory((new PropertyValueFactory<Appointments, String>("description")));
+            location.setCellValueFactory((new PropertyValueFactory<Appointments, String>("location")));
+            type.setCellValueFactory(new PropertyValueFactory<Appointments, String>("type"));
+            startDateTime.setCellValueFactory(new PropertyValueFactory<Appointments, Timestamp>("startDateTime"));
+            endDateTime.setCellValueFactory(new PropertyValueFactory<Appointments, Timestamp>("endDateTime"));
+            customerID.setCellValueFactory(new PropertyValueFactory<Appointments, Integer>("customerID"));
+            userID.setCellValueFactory(new PropertyValueFactory<Appointments, Integer>("userID"));
+            contactID.setCellValueFactory(new PropertyValueFactory<Appointments, Integer>("contactID"));
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
 
 
 }
--- src/group/model/Appointments.java
+++ src/group/model/Appointments.java
@@ -1,5 +1,8 @@
 package group.model;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 
@@ -14,7 +17,7 @@ public class Appointments {
     private int customerID;
     private int userID;
     private int contactID;
-    public static ArrayList<Appointments> apptsList = new ArrayList<Appointments>();
+    public static ObservableList<Appointments> apptsList = FXCollections.observableArrayList();
 
     public void setAppointmentID(int appointmentID) {
         this.appointmentID = appointmentID;
@@ -52,24 +55,48 @@ public class Appointments {
         this.type = type;
     }
 
+    public String getType() {
+        return type;
+    }
+
     public void setStartDateTime(Timestamp startDateTime) {
         this.startDateTime = startDateTime;
+    }
+
+    public Timestamp getStartDateTime() {
+        return startDateTime;
     }
 
     public void setEndDateTime(Timestamp endDateTime) {
         this.endDateTime = endDateTime;
     }
 
+    public Timestamp getEndDateTime() {
+        return endDateTime;
+    }
+
     public void setCustomerID(int customerID) {
         this.customerID = customerID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
     }
 
     public void setUserID(int userID) {
         this.userID = userID;
     }
 
+    public int getUserID() {
+        return userID;
+    }
+
     public void setContactID(int contactID) {
         this.contactID = contactID;
+    }
+
+    public int getContactID() {
+        return contactID;
     }
 
 
